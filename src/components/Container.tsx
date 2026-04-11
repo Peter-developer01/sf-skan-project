f747bf8b4ff9eae69367999c7ed740f621ef1ff0
Peter <101456132+Peter-developer01@users.noreply.github.com>
2026-04-11 19:23:32 +0300

    use class combination instead of class composition

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -1,4 +1,13 @@
+import classNames from "classnames";
 import styles from "../styles/Container.module.css";
-export default function Container({ children }: { children: React.ReactNode }) {
-	return <div className={styles.container}>{children}</div>;
+export default function Container({
+	children,
+	className,
+}: {
+	children: React.ReactNode;
+	className?: string;
+}) {
+	return (
+		<div className={classNames(styles.container, className)}>{children}</div>
+	);
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,15 +2,16 @@ import Logo from "./Logo";
 import Navbar from "./Navbar";
 import HeaderUserArea from "./HeaderUserArea";
 import styles from "../styles/Header.module.css";
+import Container from "./Container";
 
 export default function Header({ loggedIn }: { loggedIn: boolean }) {
 	return (
 		<header className={styles.header}>
-			<div className={styles.headerContainer}>
+			<Container className={styles.headerContainer}>
 				<Logo variant="green" className={styles.logo} />
 				<Navbar className={styles.navbar} />
 				<HeaderUserArea loggedIn={loggedIn} className={styles.headerUserArea} />
-			</div>
+			</Container>
 		</header>
 	);
 }
